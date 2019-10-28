430ea2c1e2287e06f52fc0c521feabc7a0994192
Gustavo Westarb <gustavowestarb45@gmail.com>
2019-10-27 21:19:42 -0300

    Finalizando o trabalho

--- Works/AlgorithmSearch/indexV2.py
+++ Works/AlgorithmSearch/indexV2.py
@@ -2,10 +2,21 @@ import random
 import math
 import sys
 from collections import namedtuple
+import os
+import platform
+import matplotlib.pyplot as plt
+
+
+### Alunos que desenvolveram o trabalho Bruno G. Vigentas & Gustavo Westarb ###
+
+MAX_ITERATIONS = 100
+RATE_MUTATION = 0
+POPULATION = 20
+BEST_COST = 0
+
 
 # Cria uma linha aleatoria que vai de 0 a 20
 def create_random_line(x=1, y=21 , z=20):
-    change_seed()
     return random.sample(range(x, y), z)
 
 
@@ -18,19 +29,18 @@ def create_random_matriz():
     return(matrix)
 
 
-def change_seed():
-    pass
-
-
+#Cria um número randômico entre X e Y
 def real_random(x, y):
-    change_seed()
     return random.randint(x, y)
 
 
+#Executa a função de fitness
 def _fitness(population):
-    for line in population:
-        if len(line) == 20:
-            line.append(line[0])
+
+    global BEST_COST
+
+    #Adiciona o valor da primeira coluna para última, pois no problema do caxeiro viajante, deve sempre retornar para a cidade de partida
+    population = _append_last_column(population)
 
     rand_x = create_random_line(0, 100, len(population))
     rand_y = create_random_line(0, 100, len(population))
@@ -58,11 +68,13 @@ def _fitness(population):
     dic_vector_result = {index: value for index, value in enumerate(vector_result)}
 
     # ordenando em ordem crescente e cria uma lista contendo apenas as chaves
-    dic_vector_result = sorted(dic_vector_result, key=dic_vector_result.get)
+    dic_vector_result_ordened = sorted(dic_vector_result, key=dic_vector_result.get)
+
+    BEST_COST = dic_vector_result[dic_vector_result_ordened[0]]
 
     new_population = []
     # Percorre e seleciona todos os cromossomos escolhidos
-    for idx in dic_vector_result:
+    for idx in dic_vector_result_ordened:
         without_last = list(population[idx])
 
         #Retiro o último elemento do cromossomo para nao entrar em loop, pois foi adicionado igual ao primeiro
@@ -70,14 +82,15 @@ def _fitness(population):
         new_population.append(without_last)
 
     #print(create_new_mutated_matrix(population, dic_vector_result))
-    new_population = create_new_mutated_matrix(new_population, dic_vector_result)
+    new_population = create_new_mutated_matrix(new_population, dic_vector_result_ordened)
 
     return new_population
 
 
-#Comeca o processo de mutacao
+#Comeca o processo de crossover
 def create_new_mutated_matrix(matrix, key_list):
 
+    #Seleciona os 10 primeiros items da matriz, anteriormente ordenados como melhores
     new_poppulation = matrix[:10]
 
     #Roleta
@@ -88,61 +101,61 @@ def create_new_mutated_matrix(matrix, key_list):
 
     father_indexs = []
 
+    #Escolhe os 5 primeiros pais, baseado na chance de criada na roleta acima
     for i in range(0, 5):
         indx_choice = random.choice(line_list)
         father_indexs.append(indx_choice)
         father.append(new_poppulation[indx_choice])
 
+    #Escolhe os outros 5 pais, baseado na chance de criada na roleta acima e ignorando os que já foram selecionados nos 5 primeiros pais
     for i in range(0, 5):
         indx_choice = random.choice(line_list)
         while father_indexs.__contains__(indx_choice):
             indx_choice = random.choice(line_list)
         mother.append(new_poppulation[indx_choice])
 
-    print('############################################## Matriz original ##############################################')
-    print(matrix)
+    _result_cycle = do_mutation_cycle(father, mother)
 
-    print('############################################## Pais e maes antes da mutacao ##############################################')
-    print(father)
-    print(mother)
-
-    Retorno = do_mutation_cycle(father, mother)
-
-    new_poppulation = append_in_new_poppulation(Retorno.son, new_poppulation)
-    new_poppulation = append_in_new_poppulation(Retorno.daughter, new_poppulation)
+    new_poppulation = append_in_new_poppulation(_result_cycle.son, new_poppulation)
+    new_poppulation = append_in_new_poppulation(_result_cycle.daughter, new_poppulation)
 
     return new_poppulation
 
+#Adiciona os filhos a matriz principal
 def append_in_new_poppulation(to_append, population):
     for item in to_append:
         population.append(item)
 
     return population
 
+#Inverte aleatóriamente uma coluna, do pai e mãe
 def invert_random_column(father, mother):
     random_item_chromosome = real_random(0, 19)
+
     for i in range(len(father)):
         father_value = father[i][random_item_chromosome]
         father[i][random_item_chromosome] = mother[i][random_item_chromosome]
         mother[i][random_item_chromosome] = father_value
+        
+        #Resolve os números repetidos após inversão da coluna
+        _result_vanish_repeated = vanish_repeated_value(random_item_chromosome, i, mother, father)
 
-        Retorno = vanish_repeated_value(random_item_chromosome, i, mother, father)
+        father = _result_vanish_repeated.mother
+        mother = _result_vanish_repeated.father
 
-        father = Retorno.mother
-        mother = Retorno.father
+    _resturn_mutation = _mutation(father, mother)
 
+    _result_inverted = namedtuple('_result_inverted', 'father mother')
+    _result_inverted.mother = _resturn_mutation.mother
+    _result_inverted.father = _resturn_mutation.father
 
-    Retorno = namedtuple('retorno', 'father mother')
-    Retorno.mother = mother
-    Retorno.father = father
+    return _result_inverted
 
-    return Retorno
-
+#Inverte o número duplicado do pai com a mãe, até o individuo não possui mais números repetidos
 def vanish_repeated_value(random_item_chromosome, i, mother, father):
-    repetido = False
+
     for j in range(len(father[i])):
         if j != random_item_chromosome and father[i][j] == father[i][random_item_chromosome]:
-            repetido = True
             father_value = father[i][j]
             father[i][j] = mother[i][j]
             mother[i][j] = father_value
@@ -151,86 +164,121 @@ def vanish_repeated_value(random_item_chromosome, i, mother, father):
 
             break
 
-    Retorno = namedtuple('retorno', 'mother father')
-    Retorno.father = father
-    Retorno.mother = mother
+    _result_vanish = namedtuple('_result_vanish', 'mother father')
+    _result_vanish.father = father
+    _result_vanish.mother = mother
 
-    return Retorno
+    return _result_vanish
 
+# Executa a mutação
+def _mutation(father, mother):
+    global RATE_MUTATION
+    #Inverte um cromossomo aleatório do individuo
+    for i in range(len(father)):
+        RATE_MUTATION += 1
+        _random_v1 = real_random(0, 19)
+        _random_v2 = real_random(0, 19)
 
+        _value_v1 = father[i][_random_v1]
+        father[i][_random_v1] = father[i][_random_v2]
+        father[i][_random_v2] = _value_v1
 
+    #Inverte um cromossomo aleatório do individuo
+    for i in range(len(mother)):
+        _random_v1 = real_random(0, 19)
+        _random_v2 = real_random(0, 19)
 
-def create_controll(matriz, clear_unique_values=True):
-    controll = dict()
-    for x, line in enumerate(matriz):
-        for y, value in enumerate(line):
-            key = f'{x}|{value}'
-            if not controll.get(key):
-                controll[key] = []
-            controll[key].append(y)
+        _value_v1 = mother[i][_random_v1]
+        mother[i][_random_v1] = mother[i][_random_v2]
+        mother[i][_random_v2] = _value_v1
 
-    if clear_unique_values:
-        for k, v in dict(controll).items():
-            if len(v) == 1:
-                controll.pop(k)
+    _result_mutation = namedtuple('retorno', 'mutation mother father')
+    _result_mutation.father = father
+    _result_mutation.mother = mother
 
-    return controll
-
-
-def invert_repeted_values(m_1, m_2, first):
-    controll = create_controll(m_1)
-
-    for k, v in controll.items():
-        x, value = [int(_) for _ in k.split('|')]
-
-        if first:
-            del v[0]
-        for y in v:
-            m_2_value = m_2[x][y]
-            m_2[x][y] = value
-            m_1[x][y] = m_2_value
-
-    Retorno = namedtuple('retorno', 'father, mother')
-    Retorno.father = m_1
-    Retorno.mother = m_2
-
-    print('passou')
-    return Retorno
+    return _result_mutation
 
 
+#Inicia todos os processos, crossover (cycle) e mutação
 def do_mutation_cycle(father, mother):
-    Retorno = invert_random_column(father, mother)
-    # controll = create_controll(father)
+    
+    _result_inverted_column = invert_random_column(father, mother)
 
-    father = Retorno.father
-    mother = Retorno.mother
+    father = _result_inverted_column.father
+    mother = _result_inverted_column.mother
 
-    # first = True
-    # while len(controll) > 0:
-    #     Retorno = invert_repeted_values(father, mother, first)
-    #     father = Retorno.father
-    #     mother = Retorno.mother
-    #     first = False
-    #     controll = create_controll(father)
+    _result_mutation_cycle = namedtuple('retorno', 'son daughter')
+    _result_mutation_cycle.son = father
+    _result_mutation_cycle.daughter = mother
 
-    print(' ############################################## Pai e Maes  ##############################################')
+    return _result_mutation_cycle
 
-    print(father)
-    print(mother)
-    #controll = create_controll(father)
 
-    Retorno = namedtuple('retorno', 'son daughter')
-    Retorno.son = father
-    Retorno.daughter = mother
+#Limpa o console de saida
+def _clear_console():
+    if platform.system() == 'Windows':
+        return lambda: os.system('cls')
+    else:
+        return lambda: os.system('clear')
 
-    return Retorno
 
+
+#Adiciona a última coluna igual a primeira coluna
+def _append_last_column(matrix):
+    for line in matrix:
+        if len(line) == 20:
+            line.append(line[0])
+
+    return matrix
+
+
+def _calc_mutation():
+    global RATE_MUTATION
+    RATE_MUTATION /= (MAX_ITERATIONS * POPULATION)
+    RATE_MUTATION *= 100
+
+    return RATE_MUTATION
+
+#Cria gráfico do melhor caminho
+def _show_graph(cost, population):
+    _x = []
+    _y = []
+
+    for i in population[0]:
+        _x.append(cost[i] * 20)
+        if i == 20:
+            _y.append(cost[i] * 20)
+        else:
+            _y.append(cost[i + 1] * 20)
+
+    pt = plt.subplot(111)
+    pt.scatter(_x, _y, s = 30, color = 'green', marker = '.')
+
+    _x.append(_x[0])
+    _y.append(_y[0])
+
+    plt.plot(_x, _y, color = 'purple', linestyle = 'solid', linewidth = 1)
+    plt.show()
 
 if __name__ == '__main__':
 
     matrix = create_random_matriz()
-    for i in range(10000):
-        matrix = _fitness(matrix)
+    _clear = _clear_console()
 
-    print('############################################## A matrix continua igual ?  ##############################################')
-    print(matrix)
+    for i in range(10000):
+        _clear()
+        matrix = _fitness(matrix)
+        print(f'Interação de número: {i}')
+
+    best_solution = matrix[0]
+    _rate = _calc_mutation()
+
+    # Resultado do algoritmo genetico
+    print('Tamanho da população: 20')
+    print(f'Taxa de mutação: {_rate}')
+    print('Número de cidades: 20')
+    print(f'Melhor custo: {BEST_COST}')
+    print(f'Melhor solução: {best_solution}')
+
+    matrix = _append_last_column(matrix)
+    _show_graph(matrix[0], matrix)
